41b7692d6d8db41c24c02e3ddab0db97bc7a8a3c
Matthew Conlen <mc@mathisonian.com>
2016-02-27 11:04:20 -0500

    remove stray file

--- sink.py
+++
@@ -1,8 +0,0 @@
-from .base import Node
-from gnoll.actions.nodes import update_node
-
-class SinkNode(Node):
-    def on_data(data):
-        update_node(self.attrs[id], {
-            'data': data
-        })
